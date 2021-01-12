92fae02e0379c38033497d0953d706c1c6325830
Darake <adamson.gert@hotmail.com>
2021-01-12 12:12:26 +0200

    increase client cert expiration time

--- create-certs.sh
+++ create-certs.sh
@@ -10,5 +10,5 @@ cp certs/server.crt certs/root.crt && \
 openssl genrsa -des3 -passout pass:1234 -out clientcerts/postgresql.key 1024 && \
 openssl rsa -passin pass:1234 -in clientcerts/postgresql.key -out clientcerts/postgresql.key && \
 openssl req -new -key clientcerts/postgresql.key -out clientcerts/postgresql.csr -subj '/C=FI/ST=Uusimaa/L=Helsinki/O=toska.dev/CN=$REP_USER/emailAddress=grp-toska@helsinki.fi' && \
-openssl x509 -req -in clientcerts/postgresql.csr -CA certs/root.crt -CAkey certs/server.key -out clientcerts/postgresql.crt -CAcreateserial && \
+openssl x509 -req -in clientcerts/postgresql.csr -CA certs/root.crt -CAkey certs/server.key -days 3650 -out clientcerts/postgresql.crt -CAcreateserial && \
 cp certs/root.crt clientcerts/root.crt;
